8205b7400e782a907c1e8971bb2d72435fd0d8c5
JamieM0 <jamie@jmatthews.uk>
2024-02-05 15:21:06 +0000

    * Created an Edit Extra form (standard)

---
+++ lakeside/frmEditExtra.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace lakeside
+{
+    partial class frmEditExtra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.validPricePPPN = new System.Windows.Forms.Label();
+            this.validDescription = new System.Windows.Forms.Label();
+            this.validExtraName = new System.Windows.Forms.Label();
+            this.txtPricePPPN = new lakeside.ValidationTextBox();
+            this.txtDescription = new lakeside.ValidationTextBox();
+            this.txtExtraName = new lakeside.ValidationTextBox();
+            this.lbPricePPPN = new System.Windows.Forms.Label();
+            this.lbDescription = new System.Windows.Forms.Label();
+            this.lbExtraName = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnAddCourse = new System.Windows.Forms.Button();
+            this.dgExtras = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgExtras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // validPricePPPN
+            // 
+            this.validPricePPPN.AutoSize = true;
+            this.validPricePPPN.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validPricePPPN.ForeColor = System.Drawing.Color.Red;
+            this.validPricePPPN.Location = new System.Drawing.Point(176, 307);
+            this.validPricePPPN.Name = "validPricePPPN";
+            this.validPricePPPN.Size = new System.Drawing.Size(52, 21);
+            this.validPricePPPN.TabIndex = 106;
+            this.validPricePPPN.Text = "label2";
+            // 
+            // validDescription
+            // 
+            this.validDescription.AutoSize = true;
+            this.validDescription.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validDescription.ForeColor = System.Drawing.Color.Red;
+            this.validDescription.Location = new System.Drawing.Point(176, 234);
+            this.validDescription.Name = "validDescription";
+            this.validDescription.Size = new System.Drawing.Size(52, 21);
+            this.validDescription.TabIndex = 105;
+            this.validDescription.Text = "label1";
+            // 
+            // validExtraName
+            // 
+            this.validExtraName.AutoSize = true;
+            this.validExtraName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validExtraName.ForeColor = System.Drawing.Color.Red;
+            this.validExtraName.Location = new System.Drawing.Point(176, 158);
+            this.validExtraName.Name = "validExtraName";
+            this.validExtraName.Size = new System.Drawing.Size(52, 21);
+            this.validExtraName.TabIndex = 104;
+            this.validExtraName.Text = "label1";
+            // 
+            // txtPricePPPN
+            // 
+            this.txtPricePPPN.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPricePPPN.Location = new System.Drawing.Point(180, 271);
+            this.txtPricePPPN.Name = "txtPricePPPN";
+            this.txtPricePPPN.Size = new System.Drawing.Size(105, 33);
+            this.txtPricePPPN.TabIndex = 95;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescription.Location = new System.Drawing.Point(180, 194);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(316, 33);
+            this.txtDescription.TabIndex = 94;
+            // 
+            // txtExtraName
+            // 
+            this.txtExtraName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExtraName.Location = new System.Drawing.Point(180, 122);
+            this.txtExtraName.Name = "txtExtraName";
+            this.txtExtraName.Size = new System.Drawing.Size(316, 33);
+            this.txtExtraName.TabIndex = 93;
+            // 
+            // lbPricePPPN
+            // 
+            this.lbPricePPPN.AutoSize = true;
+            this.lbPricePPPN.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPricePPPN.Location = new System.Drawing.Point(60, 274);
+            this.lbPricePPPN.Name = "lbPricePPPN";
+            this.lbPricePPPN.Size = new System.Drawing.Size(125, 25);
+            this.lbPricePPPN.TabIndex = 102;
+            this.lbPricePPPN.Text = "Extra Price:  £";
+            // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescription.Location = new System.Drawing.Point(7, 193);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(159, 25);
+            this.lbDescription.TabIndex = 101;
+            this.lbDescription.Text = "Extra Description:";
+            // 
+            // lbExtraName
+            // 
+            this.lbExtraName.AutoSize = true;
+            this.lbExtraName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbExtraName.Location = new System.Drawing.Point(53, 125);
+            this.lbExtraName.Name = "lbExtraName";
+            this.lbExtraName.Size = new System.Drawing.Size(113, 25);
+            this.lbExtraName.TabIndex = 100;
+            this.lbExtraName.Text = "Extra Name:";
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(358, 19);
+            this.lbTitle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(164, 37);
+            this.lbTitle.TabIndex = 99;
+            this.lbTitle.Text = "Edit an Extra";
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClearAll.Image = global::lakeside.Properties.Resources.returnIcon;
+            this.btnClearAll.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClearAll.Location = new System.Drawing.Point(616, 19);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(256, 65);
+            this.btnClearAll.TabIndex = 98;
+            this.btnClearAll.Text = "Clear All Fields";
+            this.btnClearAll.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnReturn.Image = global::lakeside.Properties.Resources.returnIcon;
+            this.btnReturn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReturn.Location = new System.Drawing.Point(12, 19);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(256, 65);
+            this.btnReturn.TabIndex = 97;
+            this.btnReturn.Text = "Return to Home";
+            this.btnReturn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnAddCourse
+            // 
+            this.btnAddCourse.BackgroundImage = global::lakeside.Properties.Resources.EditCourseButton;
+            this.btnAddCourse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAddCourse.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCourse.Location = new System.Drawing.Point(155, 358);
+            this.btnAddCourse.Name = "btnAddCourse";
+            this.btnAddCourse.Size = new System.Drawing.Size(262, 59);
+            this.btnAddCourse.TabIndex = 96;
+            this.btnAddCourse.UseVisualStyleBackColor = true;
+            // 
+            // dgExtras
+            // 
+            this.dgExtras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgExtras.Location = new System.Drawing.Point(518, 122);
+            this.dgExtras.Name = "dgExtras";
+            this.dgExtras.Size = new System.Drawing.Size(345, 295);
+            this.dgExtras.TabIndex = 107;
+            // 
+            // frmEditExtra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 442);
+            this.Controls.Add(this.dgExtras);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.validPricePPPN);
+            this.Controls.Add(this.validDescription);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.validExtraName);
+            this.Controls.Add(this.btnAddCourse);
+            this.Controls.Add(this.txtPricePPPN);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtExtraName);
+            this.Controls.Add(this.lbPricePPPN);
+            this.Controls.Add(this.lbDescription);
+            this.Controls.Add(this.lbExtraName);
+            this.Controls.Add(this.lbTitle);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "frmEditExtra";
+            this.Text = "frmEditExtra";
+            ((System.ComponentModel.ISupportInitialize)(this.dgExtras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Label validPricePPPN;
+        private System.Windows.Forms.Label validDescription;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Label validExtraName;
+        private System.Windows.Forms.Button btnAddCourse;
+        private ValidationTextBox txtPricePPPN;
+        private ValidationTextBox txtDescription;
+        private ValidationTextBox txtExtraName;
+        private System.Windows.Forms.Label lbPricePPPN;
+        private System.Windows.Forms.Label lbDescription;
+        private System.Windows.Forms.Label lbExtraName;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.DataGridView dgExtras;
+    }
+}
